ee33ad725a4e11a20f823d083d12b94fd87d3fb0
da413 <danielwolfcol@gmail.com>
2025-11-08 16:12:39 -0500

    linear search by name

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    List<InventoryItem> InventoryItemList = new List<InventoryItem>();
+    public static List<InventoryItem> InventoryItemList = new List<InventoryItem>();
 
     public int rand_ID;
     public float rand_Value;
 
-    public string[] nameArray = new string[10];
-
-
-    void Start()
+    
+    private string[] nArray = {"SWORD", "SHIELD", "BOW", "POTION", "EYEOFAGRESS", "WAND", "SPELLBOOK"};
+    
+   
+    void Awake()
     {
-        for(int i = 0; i<10; i++)
+        for(int i = 0; i<nArray.Length; i++)
         {
             rand_ID = Random.Range(1, 11);
             rand_Value = Random.Range(20f, 50f);
 
-            InventoryItemList.Add(new InventoryItem(rand_ID, nameArray[i], rand_Value));
+            InventoryItemList.Add(new InventoryItem(rand_ID, nArray[i], rand_Value));
 
-            Debug.Log(InventoryItemList[i].name);
+           // Debug.Log(InventoryItemList[i].name);
         }
 
         
---
+++ Assets/Scripts/LinearSearch.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearSearch : MonoBehaviour
+{
+    public string searchName;
+
+    void Start()
+    {
+        Debug.Log(LinearSearchMethod());
+        
+    }
+
+    public string LinearSearchMethod()
+    {
+        if(searchName == null)
+            return "Search is null";
+
+        foreach(InventoryItem item in InventoryManager.InventoryItemList){
+            
+            if(searchName.ToUpper().Replace(" ", string.Empty) == item.name){
+                return item.name;
+            }
+            
+        }
+        return "item not found";
+    }
+
+    
+}
